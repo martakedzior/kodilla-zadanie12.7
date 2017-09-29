65b8ac6f4b3bef8d33c6ccb73c0064a4eb2a137e
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-29 20:27:59 +0200

    Zadanie 12.7

---
+++ js/App.js
@@ -0,0 +1,33 @@
+// OGÓLNA FUNKCJA
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2329',
+  'X-Auth-Token': '50d9c987ed7125d76243244cafbd5d18'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
